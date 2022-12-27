6e55195faeff71e0d195f59e43c65f48f1414fe1
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2022-12-27 18:41:50 +0200

    added state machine logic

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,12 +1,13 @@
 import { Component } from 'react';
 // import PropTypes from 'prop-types';
-
 // import { MagnifyingGlass } from 'react-loader-spinner';
 import { fetchData } from 'components/Utils/fetchApi';
 
 export class ImageGallery extends Component {
   state = {
     status: 'idle',
+    galleryHits: [],
+    totalHits: 0,
   };
 
   async componentDidUpdate(prevProps, prevState) {
@@ -14,9 +15,37 @@ export class ImageGallery extends Component {
 
     const { currentPage, inputRequest } = this.props;
 
-    const images = await fetchData(inputRequest, currentPage);
+    if (prevProps.currentPage === currentPage && prevProps.inputRequest) {
+      return;
+    }
 
-    console.log(images);
+    this.setState({
+      status: 'pending',
+    });
+
+    const fetchedImages = await fetchData(inputRequest, currentPage);
+    console.log(fetchedImages);
+
+    if (fetchedImages === `error`) {
+      return this.setState({
+        status: 'rejected',
+      });
+    }
+
+    if (fetchedImages.hits.length === 0) {
+      return this.setState({
+        status: 'failed',
+      });
+    }
+
+    this.setState({
+      status: 'resolved',
+      totalHits: fetchedImages.totalHits,
+      galleryHits:
+        currentPage === 1
+          ? fetchedImages.hits
+          : [...this.state.galleryHits, ...fetchedImages.hits],
+    });
   }
 
   render() {
